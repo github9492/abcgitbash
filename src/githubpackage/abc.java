68c769c780e15a8b63f6a98e95f4d9990bf56910
github@9492 <gkkuswaha1@gmail.com>
2023-03-10 10:34:31 +0530

    ghkkk

---
+++ src/githubpackage/abc.java
@@ -0,0 +1,16 @@
+package githubpackage;
+
+public class abc {
+	public static void main(String arg[]) {
+		System.out.println("practice with git bash");
+		
+		System.out.println("practice with git bash");
+		System.out.println("practice with git ");
+		System.out.println("practice with git bash");
+	
+		
+		
+		
+	}
+
+}
---
+++ src/githubpackage/hgchhm.java
@@ -0,0 +1,11 @@
+package githubpackage;
+
+public class hgchhm {
+	public static void main(String arg[]) {
+		System.out.println("practice ");
+		System.out.println("practice ");
+		
+		System.out.println("practice ");
+		
+	}
+}
